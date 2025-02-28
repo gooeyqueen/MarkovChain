e0ce8fd0cd35ea7f263a1e26b7787324be0c0d19
gooeyqueen <gooeyqueen@gmail.com>
2025-02-28 21:41:19 +1000

    Created a library with basic implementation of Markov chain

---
+++ MarkovChain/Chain.cs
@@ -0,0 +1,25 @@
+﻿namespace MarkovChain
+{
+    public class Chain
+    {
+        private readonly Dictionary<string, TermsCollection> _terms = [];
+
+        public void Add(string termA, string termB)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(termA);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(termB);
+
+            if (!_terms.ContainsKey(termA))
+            {
+                _terms.Add(termA, new TermsCollection());
+            }
+
+            _terms[termA].Add(termB);
+        }
+
+        public string Next(string term)
+        {
+            return _terms[term].Next();
+        }
+    }
+}
---
+++ MarkovChain/TermsCollection.cs
@@ -0,0 +1,39 @@
+﻿namespace MarkovChain
+{
+    internal class TermsCollection
+    {
+        private static readonly Random _rng = new();
+
+        private readonly Dictionary<string, int> _terms = [];
+        private int _total;
+
+        public void Add(string term)
+        {
+            if (!_terms.ContainsKey(term))
+            {
+                _terms.Add(term, 0);
+            }
+
+            _terms[term]++;
+            _total++;
+        }
+
+        public string Next()
+        {
+            int accumulator = 0;
+            int random = _rng.Next(_total);
+
+            foreach (var termPair in _terms)
+            {
+                accumulator += termPair.Value;
+
+                if (accumulator >= random)
+                {
+                    return termPair.Key;
+                }
+            }
+
+            throw new IndexOutOfRangeException("Accumulator hasn't reached random selector");
+        }
+    }
+}
