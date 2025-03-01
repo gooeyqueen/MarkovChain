a87770f2dbea46c1c04133fab53a365f88b59d09
gooeyqueen <gooeyqueen@gmail.com>
2025-03-01 22:40:44 +1000

    Added Tokenizer project, created simple text-to-words tokenizer

---
+++ Tokenizer/TextToWordsTokenizer.cs
@@ -0,0 +1,17 @@
+﻿namespace Tokenizer
+{
+    public class TextToWordsTokenizer
+    {
+        public List<string> Tokenize(string text, IEnumerable<string> termsToRemove)
+        {
+            foreach (var term in termsToRemove)
+            {
+                text = text.Replace(term, string.Empty);
+            }
+
+            text = text.ToLower();
+
+            return text.Split().Where(w => !string.IsNullOrWhiteSpace(w)).ToList();
+        }
+    }
+}
